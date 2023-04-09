ea7aee83be0af7ac1215c50187a3cb4269b9435a
tabibabderrahmen <abdou1tabib@gmail.com>
2023-04-09 16:51:39 +0100

    remove the post created for test purpose

--- posts/index.js
+++ posts/index.js
@@ -23,13 +23,13 @@ app.post('/posts/create', async (req, res) => {
     title,
   };
 
-  await axios.post("http://event-bus-srv:4005/events", {
+ /* await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
       title,
     },
-  });
+  });*/
 
   res.status(201).send(posts[id]);
 });
@@ -43,3 +43,4 @@ app.post("/events", (req, res) => {
 app.listen(4000, () => {
   console.log("Listening on 4000");
 });
+module.exports = app;
